1d8aeaf6ae84f406433ff88b89adb7000ad36e0b
Pelayo <phuermij@myuax.com>
2023-09-26 18:38:19 +0200

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+"""
+Para este ejercicio, deberás crear una clase que representa una matriz. 
+Las operaciones que esta clase debe permitir son la creación de una matriz a partir de una lista de listas, 
+la impresión de la matriz en una forma legible, y 
+el cálculo de la transpuesta de la matriz
+"""
+
