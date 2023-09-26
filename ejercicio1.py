7526237f328ea23b2e2880840cd87a28911968d2
Pelayo <phuermij@myuax.com>
2023-09-26 19:06:31 +0200

    clases

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,21 @@ la impresión de la matriz en una forma legible, y
 el cálculo de la transpuesta de la matriz
 """
 
+class Matriz():
+    def __init__(self, elemetos: list):
+        self.elementos = elemetos
+
+class Transpuesta(Matriz):
+    def  __init__(self, elementos):
+        super().__init__(elementos)
+    
+    def trasnponer(self):
+        return Matriz([[fila[i] for fila in self.elementos] for i in range(len(self.elementos[0]))])
+    
+class Imprimir(Matriz):
+    def __init__(self, elementos):
+        super().__init__(elementos)
+    
+    def imprimir(self):
+        for fila in self.elementos:
+            print(fila)
